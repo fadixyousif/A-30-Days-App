44aa88487a1f7b590c7f36c1fcbf5552c3d0a2c3
Fadi Y <fadiyoux@gmail.com>
2026-03-20 18:16:09 -0600

    Add files with empty code template

---
+++ app/src/main/java/com/fadi/a30daysapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.fadi.a30daysapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fadi.a30daysapp.ui.theme.A30DaysAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            A30DaysAppTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    A30DaysAppTheme {
+    }
+}
